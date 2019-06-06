fe128d104a192fed1cdf2b724e42e47ceb7e6d9d
OscarFabiani <spartan539@yahoo.com>
2019-06-06 11:00:21 -0700

    Initial commit.

---
+++ myApp.js
@@ -0,0 +1,116 @@
+/*
+
+This file explores the basics of Node and Express.
+
+Node.js: Node.js is a JavaScript tool that allows developers to write backend (server-side) programs in
+JavaScript. Node.js comes with built-in modules (small, independent programs) that help facilitate this
+purpose.
+Some of the core built-in modules are:
+
+http: a module that acts as a server.
+
+fs (File System): a module that reads and modifies files.
+
+path: a module for working with directory and file paths.
+
+assert (Assertion Testing): a module that checks code against prescribed constraints.
+
+
+Express: Express is a popular module/library/framework that runs between the server created by Node.js and the
+frontend pages of a web application. Express also handles an application's routing. Routing directs users to the
+correct page based on their interaction with the application.
+
+
+Express methods: Some common Express methods are listen(), get(), and post().
+
+
+App object: The app object conventionally denotes the Express application
+
+Creating an Express app object: After requiring or importing express, an Express app object can be created
+by setting a variable to an instance of express().
+EX:
+const express = require('express');
+const app = express();
+
+
+App Methods:
+
+Listen method: The listen() method tells your server to listen on a given port, putting it in a running state.
+NOTE: This method is identical to Node's http.Server.listen().
+Syntax: app.listen([port[, host[, backlog]]][, callback])
+EX:
+//This tells the server to listen to port 3000 and includes a callback function that logs a message to the console.
+app.listen(3000, () => {console.log('app is listening on port 3000')});
+
+
+Routing Methods: Routing methods route http requests There are many, but the most common are get and post.
+Syntax: app.METHOD(PATH, HANDLER) or app.METHOD(path, callback [, callback ...])
+METHOD is an http method in lowercase, PATH is a relative path on the server, and HANDLER is a route handler
+function that Express calls when the route is matched.
+
+Get Method: Routes HTTP GET requests to the specified path with the specified callback functions.
+Syntax: app.get(PATH, HANDLER) or app.get(path, callback)
+
+
+Route handlers: Route handler functions are called when a route method's PATH is matched and take a request
+object and response object as parameters.
+Syntax: function (req, res) {...}
+
+
+Response Object (res): The res object represents the HTTP response that an Express app sends when it gets an
+HTTP request.
+
+Response Object Methods:
+
+Send: Sends the HTTP response.
+Syntax: res.send(body);
+
+
+Serving a response string to GET requests matching the root "/" path:
+EX:
+//This serves a string response when an HTTP request is made on the root path.
+app.get("/",(req, res) => {res.send("Hello Express")});
+
+
+
+
+
+*/
+
+//import express from 'express';
+const express = require('express')
+const app = express()
+
+
+//app.get('/', function (req, res) {
+  //res.send('Hello World')
+//})
+
+//app.get("/",(req, res) => {res.send("Hello Express")});
+
+let indexPath = __dirname + "/index.html";
+app.get("/", (req, res) => {res.sendFile(indexPath)});
+
+let publicPath = __dirname + "/public"
+app.use(express.static(publicPath));
+
+let data = {
+  name: "Oscar"
+}
+
+//app.get("/json", (req, res) => {res.json(data)});
+
+process.env.MESSAGE_STYLE = "uppercase";
+console.log(process.env.MESSAGE_STYLE);
+
+let jsonHandler = (req, res) => {
+  if (process.env.MESSAGE_STYLE == "uppercase") {
+    data.name = data.name.toUpperCase();
+  }
+  res.json(data);
+}
+
+app.get("/json", jsonHandler);
+
+
+app.listen(3000, () => {console.log('app is listening on port 3000')});
